0234e776fe8eadde64e1dc363e7d95204b29914f
kebele <ykemalbeler@gmail.com>
2020-08-20 03:04:09 +0300

    first

---
+++ instagram-clone/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+   <h1>home</h1>
+    
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "Home",
+  
+};
+</script>
+<style scoped>
+  h1{
+    color : red;
+
+    @media (--t) {
+      color : blue;
+    }
+    /* burada post css config i çalıştı, tablet modund ayani çöznürlük değiştiğinde renk değişecek */
+  }
+</style>
