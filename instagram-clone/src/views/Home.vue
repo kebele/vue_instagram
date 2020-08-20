dfc49b44fdab408402d0ed3ca6334f8429b4787a
kebele <ykemalbeler@gmail.com>
2020-08-20 15:13:04 +0300

    ekleme

--- instagram-clone/src/views/Home.vue
+++ instagram-clone/src/views/Home.vue
@@ -1,25 +1,27 @@
 <template>
   <div class="home">
-   <h1>home</h1>
-    
+    <h1>home</h1>
+    <CustomText>selam dunya</CustomText>
+    <CustomText tag="p">selam dunya</CustomText>
   </div>
 </template>
 
 <script>
-
-
+import CustomText from "@/components/CustomText"
 export default {
   name: "Home",
-  
-};
+  components : {
+    CustomText
+  }
+}
 </script>
 <style scoped>
-  h1{
-    color : red;
+h1 {
+  color: red;
 
-    @media (--t) {
-      color : blue;
-    }
-    /* burada post css config i çalıştı, tablet modund ayani çöznürlük değiştiğinde renk değişecek */
+  @media (--t) {
+    color: blue;
   }
+  /* burada post css config i çalıştı, tablet modund ayani çöznürlük değiştiğinde renk değişecek */
+}
 </style>
