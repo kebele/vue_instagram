30c399c305c4bc838eb8b1d9edf4be2f8ba71a74
kebele <ykemalbeler@gmail.com>
2020-08-21 17:05:23 +0300

    ekleme

--- instagram-clone/src/views/Home.vue
+++ instagram-clone/src/views/Home.vue
@@ -1,28 +1,42 @@
 <template>
   <div class="home">
     <h1>home</h1>
-    <CustomText>selam dunya</CustomText>
     <CustomText tag="p">merhaba dunya</CustomText>
-    <CustomText tag="b" size="xsmall">naber dunya</CustomText>
+    <CustomText class="a1" tag="p" size="small">naber dunya</CustomText>
+    <CustomText class="a2" tag="p" size="xsmall">naber dunya</CustomText>
+    <CustomText class="a3" tag="p" size="xxsmall">naber dunya</CustomText>
+    <IconHome width="60" height="60"/>
   </div>
 </template>
 
 <script>
 import CustomText from "@/components/CustomText"
+import IconHome from '../icons/home.svg'
+
 export default {
   name: "Home",
   components : {
-    CustomText
+    CustomText,
+    IconHome
   }
 }
 </script>
 <style scoped>
-h1 {
+/* h1 {
   color: red;
 
   @media (--t) {
     color: blue;
-  }
+  } */
   /* burada post css config i çalıştı, tablet modund ayani çöznürlük değiştiğinde renk değişecek */
+/* } */
+.a1{
+  color : rgba(var(--edc, 150,230,50),1)
+}
+.a2{
+  color : rgba(var(--j64),1)
+}
+.a3{
+  color : rgba(var(--f52),1)
 }
 </style>
