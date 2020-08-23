873bb1e0164fa9999398e426f9e321bd3896c540
kebele <ykemalbeler@gmail.com>
2020-08-23 13:23:55 +0300

    ekleme

--- instagram-clone/src/views/home/Index.vue
+++ instagram-clone/src/views/home/Index.vue
@@ -1,11 +1,27 @@
 <template>
   <div class="home">
+
+    <div class="timeline">
+
     <h1>home</h1>
     <CustomText tag="p">merhaba dunya</CustomText>
     <CustomText class="a1" tag="p" size="small">naber dunya</CustomText>
     <CustomText class="a2" tag="p" size="xsmall">naber dunya</CustomText>
     <CustomText class="a3" tag="p" size="xxsmall">naber dunya</CustomText>
     <IconHome width="60" height="60"/>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi culpa unde, autem cupiditate architecto in, illo voluptatem ipsam suscipit labore ex aliquam! In excepturi praesentium, eum vel expedita quam. Officiis, reiciendis deserunt fugiat, perspiciatis velit nobis esse rem doloribus id vitae excepturi. A, pariatur necessitatibus autem magni ab totam eius.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi culpa unde, autem cupiditate architecto in, illo voluptatem ipsam suscipit labore ex aliquam! In excepturi praesentium, eum vel expedita quam. Officiis, reiciendis deserunt fugiat, perspiciatis velit nobis esse rem doloribus id vitae excepturi. A, pariatur necessitatibus autem magni ab totam eius.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi culpa unde, autem cupiditate architecto in, illo voluptatem ipsam suscipit labore ex aliquam! In excepturi praesentium, eum vel expedita quam. Officiis, reiciendis deserunt fugiat, perspiciatis velit nobis esse rem doloribus id vitae excepturi. A, pariatur necessitatibus autem magni ab totam eius.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi culpa unde, autem cupiditate architecto in, illo voluptatem ipsam suscipit labore ex aliquam! In excepturi praesentium, eum vel expedita quam. Officiis, reiciendis deserunt fugiat, perspiciatis velit nobis esse rem doloribus id vitae excepturi. A, pariatur necessitatibus autem magni ab totam eius.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi culpa unde, autem cupiditate architecto in, illo voluptatem ipsam suscipit labore ex aliquam! In excepturi praesentium, eum vel expedita quam. Officiis, reiciendis deserunt fugiat, perspiciatis velit nobis esse rem doloribus id vitae excepturi. A, pariatur necessitatibus autem magni ab totam eius.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi culpa unde, autem cupiditate architecto in, illo voluptatem ipsam suscipit labore ex aliquam! In excepturi praesentium, eum vel expedita quam. Officiis, reiciendis deserunt fugiat, perspiciatis velit nobis esse rem doloribus id vitae excepturi. A, pariatur necessitatibus autem magni ab totam eius.</p>
+
+    </div>
+    <div class="sidebar">
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. At dolorem assumenda facere, error omnis ratione dicta quae deleniti architecto doloremque nesciunt voluptate quo saepe quidem dolore, aliquid ut. Odio dolores quae sed quibusdam similique perferendis explicabo eveniet. Iure, aperiam totam repellendus quaerat harum earum, omnis consequatur aspernatur aliquid neque ullam.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. At dolorem assumenda facere, error omnis ratione dicta quae deleniti architecto doloremque nesciunt voluptate quo saepe quidem dolore, aliquid ut. Odio dolores quae sed quibusdam similique perferendis explicabo eveniet. Iure, aperiam totam repellendus quaerat harum earum, omnis consequatur aspernatur aliquid neque ullam.</p>
+    </div>
+
   </div>
 </template>
 
@@ -30,7 +46,7 @@ export default {
   } */
   /* burada post css config i çalıştı, tablet modund ayani çöznürlük değiştiğinde renk değişecek */
 /* } */
-.a1{
+/* .a1{
   color : rgba(var(--edc, 150,230,50),1)
 }
 .a2{
@@ -38,5 +54,24 @@ export default {
 }
 .a3{
   color : rgba(var(--f52),1)
+} */
+.home{
+  max-width: 605px;
+  margin-left: auto;
+  margin-right: auto;
+  @media (--t){
+    max-width: none;
+    display: grid;
+    grid-template-columns: 1fr 295px;
+    grid-gap: 30px;
+    
+  }
+}
+
+.sidebar {
+  display: none;
+  @media (--t) {
+    display: block;
+  }
 }
 </style>
