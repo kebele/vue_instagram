30133cd1164aea9336b7e574edd43605d364ba19
kebele <ykemalbeler@gmail.com>
2020-08-23 22:08:42 +0300

    ekleme

---
+++ instagram-clone/src/components/Avatar.vue
@@ -0,0 +1,42 @@
+<template>
+  <figure class="avatar">
+    <img :src="src" :alt="alt" :width="size" :height="size" />
+  </figure>
+</template>
+
+<script>
+export default {
+  name: "Avatar",
+  props: {
+    src: {
+      type: String,
+      default:
+        "https://instagram.fesb3-1.fna.fbcdn.net/v/t51.2885-19/s150x150/118255309_118179853130935_754774801536933337_n.jpg?_nc_ht=instagram.fesb3-1.fna.fbcdn.net&_nc_ohc=BUk_HBpNthQAX9nAEjm&oh=dc20022660cc415e8cd66379b7abf50b&oe=5F6D2C78",
+    },
+    alt: {
+      type: String,
+    },
+    size: {
+      type: Number,
+    },
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+.avatar {
+  display: inline-flex;
+  padding: 3px;
+  border-radius: 999px;
+  background : 
+  img {
+    border-radius: inherit;
+    border : 2px solid white;
+  }
+}
+</style>
+
+
+
+
+
--- instagram-clone/src/views/home/Index.vue
+++ instagram-clone/src/views/home/Index.vue
@@ -4,36 +4,30 @@
     <div class="timeline">
 
     <h1>home</h1>
-    <CustomText tag="p">merhaba dunya</CustomText>
-    <CustomText class="a1" tag="p" size="small">naber dunya</CustomText>
-    <CustomText class="a2" tag="p" size="xsmall">naber dunya</CustomText>
-    <CustomText class="a3" tag="p" size="xxsmall">naber dunya</CustomText>
-    <IconHome width="60" height="60"/>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi culpa unde, autem cupiditate architecto in, illo voluptatem ipsam suscipit labore ex aliquam! In excepturi praesentium, eum vel expedita quam. Officiis, reiciendis deserunt fugiat, perspiciatis velit nobis esse rem doloribus id vitae excepturi. A, pariatur necessitatibus autem magni ab totam eius.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi culpa unde, autem cupiditate architecto in, illo voluptatem ipsam suscipit labore ex aliquam! In excepturi praesentium, eum vel expedita quam. Officiis, reiciendis deserunt fugiat, perspiciatis velit nobis esse rem doloribus id vitae excepturi. A, pariatur necessitatibus autem magni ab totam eius.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi culpa unde, autem cupiditate architecto in, illo voluptatem ipsam suscipit labore ex aliquam! In excepturi praesentium, eum vel expedita quam. Officiis, reiciendis deserunt fugiat, perspiciatis velit nobis esse rem doloribus id vitae excepturi. A, pariatur necessitatibus autem magni ab totam eius.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi culpa unde, autem cupiditate architecto in, illo voluptatem ipsam suscipit labore ex aliquam! In excepturi praesentium, eum vel expedita quam. Officiis, reiciendis deserunt fugiat, perspiciatis velit nobis esse rem doloribus id vitae excepturi. A, pariatur necessitatibus autem magni ab totam eius.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi culpa unde, autem cupiditate architecto in, illo voluptatem ipsam suscipit labore ex aliquam! In excepturi praesentium, eum vel expedita quam. Officiis, reiciendis deserunt fugiat, perspiciatis velit nobis esse rem doloribus id vitae excepturi. A, pariatur necessitatibus autem magni ab totam eius.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi culpa unde, autem cupiditate architecto in, illo voluptatem ipsam suscipit labore ex aliquam! In excepturi praesentium, eum vel expedita quam. Officiis, reiciendis deserunt fugiat, perspiciatis velit nobis esse rem doloribus id vitae excepturi. A, pariatur necessitatibus autem magni ab totam eius.</p>
+    <Avatar size="40"></Avatar>
+    
+    
 
     </div>
     <div class="sidebar">
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. At dolorem assumenda facere, error omnis ratione dicta quae deleniti architecto doloremque nesciunt voluptate quo saepe quidem dolore, aliquid ut. Odio dolores quae sed quibusdam similique perferendis explicabo eveniet. Iure, aperiam totam repellendus quaerat harum earum, omnis consequatur aspernatur aliquid neque ullam.</p>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. At dolorem assumenda facere, error omnis ratione dicta quae deleniti architecto doloremque nesciunt voluptate quo saepe quidem dolore, aliquid ut. Odio dolores quae sed quibusdam similique perferendis explicabo eveniet. Iure, aperiam totam repellendus quaerat harum earum, omnis consequatur aspernatur aliquid neque ullam.</p>
+      
     </div>
 
   </div>
 </template>
 
 <script>
-import CustomText from "@/components/CustomText"
-import IconHome from '@/icons/home.svg'
+// import CustomText from "@/components/CustomText"
+// import IconHome from '@/icons/home.svg'
+import Avatar from '@/components/Avatar'
 
 export default {
   name: "Home",
   components : {
-    CustomText,
-    IconHome
+    // CustomText,
+    // IconHome,
+    Avatar,
   }
 }
 </script>
