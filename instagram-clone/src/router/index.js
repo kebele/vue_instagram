008bc153aee274d0695da1e0d8370d3101ac311c
kebele <ykemalbeler@gmail.com>
2020-08-23 14:54:30 +0300

    ekleme

--- instagram-clone/src/router/index.js
+++ instagram-clone/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "@/views/home/index.vue";
+// import Home from "../views/home/index.vue";
+import Home from "../views/home/index.vue";
 
 Vue.use(VueRouter);
 
